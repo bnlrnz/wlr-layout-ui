81fef114f5978c07b0e9adfd2452b5c0ac56fdc7
Ben Lorenz <bnlrnz@gmail.com>
2024-05-07 13:57:51 +0200

    added nix shell for development

---
+++ shell.nix
@@ -0,0 +1,25 @@
+with import <nixpkgs> {};
+
+pkgs.mkShell {
+
+  nativeBuildInputs = with pkgs; [
+    poetry
+    python311Packages.pyglet
+    python311Packages.tomli
+    python311Packages.tomli-w
+    wlr-randr
+    libGL
+    xorg.libX11
+    freetype
+    fontconfig
+    xorg.xrandr # for X11 support
+  ];
+
+  LD_LIBRARY_PATH = lib.makeLibraryPath [ libGL xorg.libX11 freetype fontconfig ];
+
+  shellHook = ''
+    echo "Install dependencies: poetry install"
+    echo "Build: poetry build"
+    echo "Run: poetry run wlrlui"
+  '';
+}
